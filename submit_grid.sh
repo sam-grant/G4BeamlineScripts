aadffd12cdf3f905bd08c65cc7ee362c9b1bb0ba
Samuel Grant <sgrant@fnal.gov>
2023-09-21 16:50:19 -0500

    First commit to fork: added test job option to submit_grid.sh

--- submit_grid.sh
+++ submit_grid.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [[ $1 != 0 && $1 != 1 && $1 != 2 ]] 
+if [[ $1 != -1 && $1 != 0 && $1 != 1 && $1 != 2 ]] 
 then
     echo "No arguments supplied. Use the following options:"
+    echo "Submit a test job:   ./submit_grid.sh -1 Mu2e.in"
     echo "Submit stage 1 jobs:   ./submit_grid.sh 0 Mu2e.in"
     echo "Submit PS jobs:        ./submit_grid.sh 1 Mu2e.in"
     echo "Submit Beam jobs:      ./submit_grid.sh 2 Mu2e.in"
@@ -24,17 +25,18 @@ cp $2 ${mu2ein}
 
 run_offsite=0
 opt_offsite=""
-if [ ${run_offsite} == 1 ] 
-then
+
+if [ ${run_offsite} == 1 ]; then
     opt_offsite=" --prestage-spec=prestage.txt --OS=SL6 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE --site=Caltech,FERMIGRID,FNAL,MIT,MWT2,Michigan,Nebraska,Omaha,SU-OG,Wisconsin,UCSD"
 fi
 
 
-if [ $1 == 0 ] 
-then
-    command="mu2eg4bl --in=${mu2ein} --tar=${tarbal} --njobs=200 --events-per-job=5000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1' ${opt_offsite}"
-else
-    command="mu2eg4bl --in=${mu2ein} --tar=${tarbal} --njobs=250 --events-per-job=5000000 --g4bl-version=v2_16 --g4bl-add-args='READ_Beam_File=$1' ${opt_offsite}"
+if [ $1 == 0 ]; then
+    command="mu2eg4bl --in=${mu2ein} --tar=${tarbal} --njobs=200 --events-per-job=5000 --g4bl-version=v3_06 --g4bl-add-args='READ_Beam_File=$1' ${opt_offsite}"
+elif [ $1 == 1 ]; then
+    command="mu2eg4bl --in=${mu2ein} --tar=${tarbal} --njobs=250 --events-per-job=5000000 --g4bl-version=v3_06 --g4bl-add-args='READ_Beam_File=$1' ${opt_offsite}"
+elif [ $1 == -1 ]; then # Test job
+    command="mu2eg4bl --in=${mu2ein} --tar=${tarbal} --njobs=1 --events-per-job=1000 --g4bl-version=v3_06 --g4bl-add-args='READ_Beam_File=$1' ${opt_offsite}"
 fi
 
 echo "Executing:" $command
